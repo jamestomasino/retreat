75c31f6689fe1ebe12999500d42e224dfbb2eb19
James Tomasino <james@tomasino.org>
2014-09-05 13:30:19 -0400

    working bg cover images

--- index.php
+++ index.php
@@ -4,11 +4,23 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 		<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
 		<meta name="apple-mobile-web-app-capable" content="yes" />
-		<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 		<title>Instant Retreat</title>
 
 		<style type="text/css">
-			h1{text-align:center; color:red;}
+			html {
+				<?php
+					$imagesDir = 'images/';
+					$images = glob($imagesDir . '*.{gif}', GLOB_BRACE);
+					$randomImage = $images[array_rand($images)];
+					print "background: url(" . $randomImage . ") no-repeat center center fixed;";
+				?>
+
+				-webkit-background-size: cover;
+				-moz-background-size: cover;
+				-o-background-size: cover;
+				background-size: cover;
+			}
 		</style>
 	</head>
 	<body>
