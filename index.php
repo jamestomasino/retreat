300296639d9897be6566044d597289f58d9695d3
James Tomasino <james@tomasino.org>
2014-09-05 13:14:58 -0400

    adding images and stubbed out index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+		<title>Instant Retreat</title>
+
+		<style type="text/css">
+			h1{text-align:center; color:red;}
+		</style>
+	</head>
+	<body>
+	</body>
+</html>
